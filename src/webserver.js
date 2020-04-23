5ba99d8d2dcad3f6dca81d3d3cbbee5cd2c1cda1
craftycram <github@craftycram.net>
2020-04-23 12:35:57 +0200

    created webserver

---
+++ src/webserver.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// viewed at http://localhost:8080
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/index.html`));
+});
+
+app.listen(8080);
